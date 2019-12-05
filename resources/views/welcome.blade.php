1d14221de7c00239874774769364926b4765e217
Yevhenii <panchenkoyevhenii@gmail.com>
2019-12-05 15:27:37 +0200

    update 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Update
+                    Laravel Update 2
                 </div>
 
                 <div class="links">
