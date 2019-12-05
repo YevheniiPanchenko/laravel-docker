ada954e6153e98f6c5c7fcf9fe4dc16380c19af9
Yevhenii <panchenkoyevhenii@gmail.com>
2019-12-05 15:10:49 +0200

    update docker test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Update
                 </div>
 
                 <div class="links">
